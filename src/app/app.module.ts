6b298aaa13ec4750cf5a41d27cb4636c3dc4bcc5
Ngum <ngumbutlar43@gmail.com>
2023-05-24 23:27:55 +0100

    Created Angular project for Unigist Front-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogViewComponent } from './components/blog-view/blog-view.component';
+import { BloggerComponent } from './components/blogger/blogger.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogViewComponent,
+    BloggerComponent,
+    SigninComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/blogger/blogger.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blogger',
+  templateUrl: './blogger.component.html',
+  styleUrls: ['./blogger.component.css']
+})
+export class BloggerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

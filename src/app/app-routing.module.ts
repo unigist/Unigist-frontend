ba8b41af246c42b13d93114981d2f615c1ee22e7
Ngum <ngumbutlar43@gmail.com>
2023-05-24 23:55:08 +0100

    Feat: Added Route modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BloggerComponent } from './components/blogger/blogger.component';
+import { BlogViewComponent } from './components/blog-view/blog-view.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SigninComponent } from './components/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:  '', pathMatch: 'full', redirectTo: '/home'},
+  {path: 'home', component:BlogViewComponent},
+  {path: 'blogger', component:BloggerComponent},
+  {path: 'signin', component:SigninComponent},
+  {path: "**", component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
